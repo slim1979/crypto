9c59ab128d55fc3031335ac619a86719da67f4c1
slim1979 <chinezan@yandex.ru>
2018-05-24 21:50:30 +0300

    have many trades, tickers, candles, symbols presence validation added

--- app/models/pair.rb
+++ app/models/pair.rb
@@ -1,2 +1,7 @@
 class Pair < ApplicationRecord
+  has_many :trades, dependent: :destroy
+  has_many :candles, dependent: :destroy
+  has_many :tickers, dependent: :destroy
+
+  validates :symbols, presence: true
 end
