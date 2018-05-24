bb53aa0d51a28e4d53b9e977a7ff9e28512b51a6
slim1979 <chinezan@yandex.ru>
2018-05-24 22:35:21 +0300

    tests passed

--- spec/models/pair_spec.rb
+++ spec/models/pair_spec.rb
@@ -2,6 +2,13 @@ require 'rails_helper'
 
 RSpec.describe Pair, type: :model do
   it { should validate_presence_of :symbols }
+  it { should validate_presence_of :baseCurrency }
+  it { should validate_presence_of :quoteCurrency }
+  it { should validate_presence_of :quantityIncrement }
+  it { should validate_presence_of :tickSize }
+  it { should validate_presence_of :takeLiquidityRate }
+  it { should validate_presence_of :provideLiquidityRate }
+  it { should validate_presence_of :feeCurrency }
   it { should have_many :candles }
   it { should have_many :trades }
 end
