bcb28589eacd459f7d3aa426699f03c48dc61821
slim1979 <chinezan@yandex.ru>
2018-05-25 12:41:11 +0300

    get #index spec passed

--- spec/controllers/pairs_controller_spec.rb
+++ spec/controllers/pairs_controller_spec.rb
@@ -2,4 +2,17 @@ require 'rails_helper'
 
 RSpec.describe PairsController, type: :controller do
 
+  let(:pairs) { create_list(:pair, 6) }
+
+  describe 'GET #index' do
+    before { get :index }
+
+    it 'populates an array of all questions' do
+      expect(assigns(:pairs)).to match_array(pairs)
+    end
+
+    it 'will render index template' do
+      expect(response).to render_template :index
+    end
+  end
 end
