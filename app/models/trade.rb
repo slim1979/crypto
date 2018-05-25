ac7ffa754bd20b516cd3c7967e7f5ed8c0b76e35
slim1979 <chinezan@yandex.ru>
2018-05-25 16:53:46 +0300

    validations added

--- app/models/trade.rb
+++ app/models/trade.rb
@@ -1,3 +1,12 @@
 class Trade < ApplicationRecord
   belongs_to :pair
+
+  validates :trade_id,
+            :price,
+            :quantity,
+            :side,
+            :timestamp,
+            presence: true
+  validates :trade_id, uniqueness: true
+
 end
--- spec/models/trade_spec.rb
+++ spec/models/trade_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Trade, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to :pair }
+  it { should validate_presence_of :trade_id }
+  it { should validate_presence_of :price }
+  it { should validate_presence_of :quantity }
+  it { should validate_presence_of :side }
+  it { should validate_presence_of :timestamp }
+  it { should validate_uniqueness_of :trade_id }
+
 end
