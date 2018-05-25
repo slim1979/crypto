40d130465f68337abe4fc3763c411f1542653d92
slim1979 <chinezan@yandex.ru>
2018-05-25 17:08:33 +0300

    added history, load_pair, before action

--- app/controllers/pairs_controller.rb
+++ app/controllers/pairs_controller.rb
@@ -1,9 +1,17 @@
 class PairsController < ApplicationController
+  before_action :load_pair, except: :index
+
   def index
     @pairs = Pair.all
   end
 
-  def show
+  def show; end
+
+  def history; end
+
+  private
+
+  def load_pair
     @pair = Pair.find params[:id]
   end
 end
