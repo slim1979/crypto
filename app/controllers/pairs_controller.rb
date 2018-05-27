ca1546eaae39b60b78df90b7eafac5777ba00467
slim1979 <chinezan@yandex.ru>
2018-05-27 19:02:40 +0300

    history renamed to candles

--- app/controllers/pairs_controller.rb
+++ app/controllers/pairs_controller.rb
@@ -7,11 +7,12 @@ class PairsController < ApplicationController
 
   def show; end
 
-  def history; end
+  def candles; end
 
   private
 
   def load_pair
     @pair = Pair.find params[:id]
+    @candles = @pair.candles
   end
 end
