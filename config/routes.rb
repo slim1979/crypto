eb07dfb63d681e60142f7a61ceaa524208d3b86a
slim1979 <chinezan@yandex.ru>
2018-05-27 19:02:20 +0300

    redudant resources routes removed

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
-  resources :orderbooks
-  resources :tickers
-  resources :trades
-  resources :candles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :pairs, only: %i[index create show] do
-    get :history, on: :member
+    get :candles, on: :member
   end
 end
