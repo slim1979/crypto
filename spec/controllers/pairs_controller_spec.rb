5bd6efe820a8ea63d421ba2038d54a3c38afd18d
slim1979 <chinezan@yandex.ru>
2018-05-27 19:03:38 +0300

    history renamed to candles, expectation added

--- spec/controllers/pairs_controller_spec.rb
+++ spec/controllers/pairs_controller_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe PairsController, type: :controller do
 
   let(:pair)   { create(:pair) }
   let(:pairs) { create_list(:pair, 6) }
+  let(:candles) { create_list(:candle, 6, pair: pair) }
 
   describe 'GET #index' do
     before { get :index }
@@ -29,15 +30,16 @@ RSpec.describe PairsController, type: :controller do
     end
   end
 
-  describe 'GET #history' do
-    before { get :history, params: { id: pair } }
+  describe 'GET #candles' do
+    before { get :candles, params: { id: pair } }
 
     it 'assign request pair to @pair' do
       expect(assigns(:pair)).to eq pair
+      expect(assigns(:candles)).to match_array(candles)
     end
 
     it 'will render history template' do
-      expect(response).to render_template :history
+      expect(response).to render_template :candles
     end
   end
 end
