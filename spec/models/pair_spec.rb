f6984c14b31639321a3b7199733db5d0840836be
slim1979 <chinezan@yandex.ru>
2018-05-28 16:11:14 +0300

    orderbook separated on 2, tests passed

--- spec/models/pair_spec.rb
+++ spec/models/pair_spec.rb
@@ -11,18 +11,22 @@ RSpec.describe Pair, type: :model do
   it { should validate_presence_of :feeCurrency }
   it { should have_many(:candles).dependent(:destroy) }
   it { should have_many(:trades).dependent(:destroy) }
-  it { should have_one(:orderbook).dependent(:destroy) }
+  it { should have_many(:orderbook_asks).dependent(:destroy) }
+  it { should have_many(:orderbook_bids).dependent(:destroy) }
   it { should validate_uniqueness_of :symbols }
 
   describe 'daily check for new pairs' do
     let(:pair)   { create(:pair, created_at: Time.current.localtime.yesterday.beginning_of_day ) }
     let(:trade)  { create(:trade, pair: pair) }
     let(:candle) { create(:candle, pair: pair) }
+    let(:orderbook_asks) { create_list(:orderbook_ask, 6, pair: pair) }
+    let(:orderbook_bids) { create_list(:orderbook_bid, 6, pair: pair) }
 
     it 'will check for new pair' do
       expect(Pair).to receive('check_for_new_pairs').and_return(Array)
       expect(Pair).to receive('check_for_trades').and_return(Array)
       expect(Pair).to receive('check_for_candles').and_return(Array)
+      expect(Pair).to receive('check_for_orderbook').and_return(Array)
       Pair.check_shedule
     end
   end
