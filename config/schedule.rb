17c4a22d1a3f55d98519d382e80f726a3e43bd8e
slim1979 <chinezan@yandex.ru>
2018-05-28 18:13:13 +0300

    added 1 time per day runner

--- config/schedule.rb
+++ config/schedule.rb
@@ -13,8 +13,8 @@
 #   rake "some:great:rake:task"
 # end
 #
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
+every 1.days do
+  runner 'CheckDataJob.perform_later'
+end
 
 # Learn more: http://github.com/javan/whenever
